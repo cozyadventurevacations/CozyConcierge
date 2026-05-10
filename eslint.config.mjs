a4b6d26c549070d5eb6d3cf7b6b27e58efcf9d38
Jeremy Brown <cozyadventurevacations@gmail.com>
2026-05-10 14:02:06 -0500

    Silence no-explicit-any warnings, clean build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@next/next/no-html-link-for-pages": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn"
     }
   }
